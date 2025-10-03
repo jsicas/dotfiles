7180971e0af8d8c610ab5edd3485413153e0911d
jsicas <jvsiqueira2013@gmail.com>
2025-10-03 00:59:37 -0300

    muitas mudanças

--- pos_install.sh
+++ pos_install.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-# script para automatizar a pós instalação
+# script para automatizar a pós instalação do popOS 22
 
-# instalando aplicativos --------------------------------------------
-# atualizando reposítorio e pacotes
+set -e  # exit on error
+
+echo '========== Iniciando Pós-Intalação =========='
+
+# definindo arrays de aplicativos para instalar, extensões para desabilitar...
+
+# apps para instalar
+apt_apps=(gnome-software )
+apt_s76=(discord steam)  # depende dos empacotamentos da System76
+
+# extensões para desabilitar
+gnome_extension_disable=(
+    ding@rastersoft.com     # retira icones do Desktop
+    pop-shell@system76.com  # retira icone de tili windows 
+)
+
+
+# atualização e download de apps -------------------------
+# apps disponíveis no apt
+echo 'Atualizando repositórios...'
 sudo apt update
-sudp apt upgrade -y
+sudo apt upgrade -y
 
-# loja
-sudo apt install gnome-software
+echo 'Download dos aplicativos...'
+sudo apt install ${apt_apps[@]} -y
+sudo apt install ${apt_s76[@]} -y
 
-# instalando aplicativos
-sudo apt install discord  # depende do empacotamento da system76
+# demais apps
+
+
+# baixando e desabilitando extensões ----------------------
+echo 'Configurando extensões...'
+
+# baixando
+
+# configurando
+
+# desabilitando
+for extension in ${gnome_extension_disable[@]}; do
+    gnome-extensions disable "$extension"
+done
+
+
+echo 'Fim da Pós-Instalação.'
