b6b43313e3765be16cb28e610d8f40c09a717ee5
jsicas <jvsiqueira2013@gmail.com>
2025-10-18 19:45:54 -0300

    Update mk_config e bash_profile

--- .bash_profile
+++ .bash_profile
@@ -6,8 +6,6 @@ if [ -f ~/.bashrc ]; then
 fi
 
 # User specific environment and startup programs
-
-# add TeX Live 2025 to PATH
-export PATH="$HOME/.texlive/2025/bin/x86_64-linux:$PATH"
-export MANPATH="$HOME/.texlive/2025/texmf-dist/doc/man:$MANPATH"
-export INFOPATH="$HOME/.texlive/2025/texmf-dist/doc/info:$INFOPATH"
+export PATH=/usr/local/texlive/2025/bin/x86_64-linux:$PATH
+export MANPATH=/usr/local/texlive/2025/texmf-dist/doc/man:$MANPATH
+export INFOPATH=/usr/local/texlive/2025/texmf-dist/doc/info:$INFOPATH
--- mk_config.sh
+++ mk_config.sh
@@ -3,9 +3,8 @@
 # criando links simbólicos
 ln -sf ~/.dotfiles/.XCompose ~/.XCompose          # atalhos de teclado personalizados
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc                # shortcurts personalizados no vim
+ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile  # executa no login
 #ln -sf ~/.dotfiles/.bashrc ~/.bashrc              # executa sempre que o terminal abre
-#ln -sf ~/.dotfiles/.profile ~/.profile            # executado no login quando .bash_profile não existir
-#ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile  # executando no login
 
 # configurações do Rstudio
 if command -v rstudio &> /dev/null; then
