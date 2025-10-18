d1f1123a51a385cb0300d7b6e97a0ca7fdcf1283
jsicas <jvsiqueira2013@gmail.com>
2025-10-18 17:01:14 -0300

    Update mk_config

--- mk_config.sh
+++ mk_config.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # criando links simbólicos
+ln -sf ~/.dotfiles/.XCompose ~/.XCompose          # atalhos de teclado personalizados
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc                # shortcurts personalizados no vim
 #ln -sf ~/.dotfiles/.bashrc ~/.bashrc              # executa sempre que o terminal abre
 #ln -sf ~/.dotfiles/.profile ~/.profile            # executado no login quando .bash_profile não existir
-ln -sf ~/.dotfiles/.XCompose ~/.XCompose          # atalhos de teclado personalizados
 #ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile  # executando no login
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc                # shortcurts personalizados no vim
 
+# configurações do Rstudio
 if command -v rstudio &> /dev/null; then
-    ln -f ~/.dotfiles/rstudio-prefs.json ~/.config/rstudio/rstudio-prefs.json       # configurações do Rstudio
+    ln -f ~/.dotfiles/rstudio-prefs.json ~/.config/rstudio/rstudio-prefs.json
 fi
