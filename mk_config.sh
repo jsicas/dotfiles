7a8f471582a98d2ece75edba8e555064265789ba
jsicas <jvsiqueira2013@gmail.com>
2025-11-19 23:39:56 -0300

    starship e bashrc

--- mk_config.sh
+++ mk_config.sh
@@ -4,7 +4,8 @@
 ln -sf ~/.dotfiles/.XCompose ~/.XCompose          # atalhos de teclado personalizados
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc                # shortcurts personalizados no vim
 ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile  # executa no login
-#ln -sf ~/.dotfiles/.bashrc ~/.bashrc              # executa sempre que o terminal abre
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc              # executa sempre que o terminal abre
+ln -sf ~/.dotfiles/starship.toml ~/.config/starship.toml
 
 # configurações do Rstudio
 if command -v rstudio &> /dev/null; then
