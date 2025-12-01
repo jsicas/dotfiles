2437ee3d180107110dfe3e068d3621d6eabc3a11
jsicas <jvsiqueira2013@gmail.com>
2025-11-30 23:37:05 -0300

    Update mk_config

--- mk_config.sh
+++ mk_config.sh
@@ -6,7 +6,11 @@ ln -sf ~/.dotfiles/.vimrc ~/.vimrc                # shortcurts personalizados no
 ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile  # executa no login
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc              # executa sempre que o terminal abre
 ln -sf ~/.dotfiles/starship.toml ~/.config/starship.toml
-ln -sf ~/.dotfiles/settings.json ~/.config/Code/User/settings.json  # VSCode
+
+# VSCode
+if command -v rstudio &> /dev/null; then
+    ln -sf ~/.dotfiles/settings.json ~/.config/Code/User/settings.json
+fi
 
 # configurações do Rstudio
 if command -v rstudio &> /dev/null; then
